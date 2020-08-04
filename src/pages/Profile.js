6395752ad448bfac974a8aab6577d8149be71728
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2020-08-04 09:20:17 -0500

    Profile Modified

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,65 @@
 import React from "react"
 import { Link } from "@reach/router"
 import "../styles/App.scss"
+import profile from "../static/construction.jpg"
 //Components
 import Header from "../components/Header.jsx"
 import Footer from "../components/Footer.jsx"
+
+const ProfileImg = () => {
+  return (
+    <div className="profile-container">
+      <img className="profile-img" src={profile} alt="profile-img" />
+    </div>
+  )
+}
+
+const ProfileBox = () => {
+  return (
+    <div className="box">
+      <div className="category-1">
+        <p>
+          Supervisor: <span>Joaquín Gutierrez Ortega</span>
+        </p>
+        <p>
+          Avance Físico: <span>23%</span>
+        </p>
+      </div>
+      <div className="category-2">
+        <p>
+          Supervisor en seguridad e higiene: <span>Ariana Gómez Cruz</span>
+        </p>
+        <p>
+          Avance Fínanciero: <span>28%</span>
+        </p>
+      </div>
+      <div className="category-3">
+        <p>
+          Número de Contrato: <span>15432</span>
+        </p>
+        <p>
+          Indice de Cumplimiento: <span>1</span>
+        </p>
+      </div>
+      <div className="category-4">
+        <p>
+          Otra Información: <span>Contenido</span>
+        </p>
+        <p>
+          Porcentaje de Avance: <span>25%</span>
+        </p>
+      </div>
+    </div>
+  )
+}
 
 const Profile = () => {
   return (
     <>
       <Header />
-      <h1>I'm Profile view</h1>
+      <h1>Nombre de la Empresa | Nombre de la Obra</h1>
+      <ProfileImg />
+      <ProfileBox />
       <Footer />
     </>
   )
